b9869bc507688d36dd0486c461b45f1dd56b794d
joseaparici <jo.aparici@ext.desigual.com>
2019-05-06 11:59:03 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mms-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'MyMovieSearch';
+}
